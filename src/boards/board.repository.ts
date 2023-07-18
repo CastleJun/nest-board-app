0db485297ab062aceaa732a61e50affe35ab178a
dev.jun28 <hsj9360@gmail.com>
2023-07-19 01:48:20 +0900

    refactor: add board repository

---
+++ src/boards/board.repository.ts
@@ -0,0 +1,14 @@
+import { DataSource, Repository } from 'typeorm';
+import { Board } from './board.entity';
+import { Injectable } from '@nestjs/common';
+
+@Injectable()
+export class BoardRepository extends Repository<Board> {
+  constructor(dataSource: DataSource) {
+    super(Board, dataSource.createEntityManager());
+  }
+
+  async getBoardById(id: number) {
+    return await this.findOneBy({ id: id });
+  }
+}
