474cfe1865292da1cd90d087dba6af32047fef0b
dev.jun28 <hsj9360@gmail.com>
2023-07-26 02:10:08 +0900

    refactor: 아이디 생성시 중복 아이디 확인 로직 추가

--- src/auth/user.repository.ts
+++ src/auth/user.repository.ts
@@ -3,12 +3,23 @@ import { CustomRepository } from '../typeorm-util/typeorm-ex.decorator';
 import { User } from './user.entity';
 import { Repository } from 'typeorm';
 import { AuthCredentialDto } from './dto/auth-credential.dto';
+import {
+  ConflictException,
+  InternalServerErrorException,
+} from '@nestjs/common';
 
 @CustomRepository(User)
 export class UserRepository extends Repository<User> {
   async createUser(authCredentialDto: AuthCredentialDto): Promise<void> {
     const user = this.create(authCredentialDto);
 
-    await this.save(user);
+    try {
+      await this.save(user);
+    } catch (error) {
+      if (error.code === '23505') {
+        throw new ConflictException('중복된 유저 이름입니다.');
+      }
+      throw new InternalServerErrorException();
+    }
   }
 }
