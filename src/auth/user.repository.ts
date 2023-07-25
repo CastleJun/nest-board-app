e7aee33a45a575e4945517c6f6d2184a2730135b
dev.jun28 <hsj9360@gmail.com>
2023-07-26 02:21:11 +0900

    refactor: 아이디 생성시 password 암호화 추가

--- src/auth/user.repository.ts
+++ src/auth/user.repository.ts
@@ -8,18 +8,26 @@ import {
   InternalServerErrorException,
 } from '@nestjs/common';
 
+import * as bcrypt from 'bcryptjs';
+
 @CustomRepository(User)
 export class UserRepository extends Repository<User> {
   async createUser(authCredentialDto: AuthCredentialDto): Promise<void> {
-    const user = this.create(authCredentialDto);
+    const { password, username } = this.create(authCredentialDto);
+
+    const salt = await bcrypt.genSalt();
+    const hashedPassword = await bcrypt.hash(password, salt);
+
+    const user = this.create({ username, password: hashedPassword });
 
     try {
       await this.save(user);
     } catch (error) {
       if (error.code === '23505') {
         throw new ConflictException('중복된 유저 이름입니다.');
+      } else {
+        throw new InternalServerErrorException();
       }
-      throw new InternalServerErrorException();
     }
   }
 }
